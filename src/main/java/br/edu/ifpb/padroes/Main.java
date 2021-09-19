17167007ec1138b6c9977bdaa63046a27462f9ee
carlosnsilva <cmanoel17@gmail.com>
2021-09-19 15:56:51 -0300

    Subindo ajuste

--- src/main/java/br/edu/ifpb/padroes/Main.java
+++ src/main/java/br/edu/ifpb/padroes/Main.java
@@ -34,7 +34,7 @@ public class Main {
         Pizza pizza = new PizzaShopPizza("pepperoni", 55.0f);
 
         // TODO - implementar padrão decorator para só precisar passar o objeto pizza
-        pizzaShopService.orderPizza(pizza, true, true, false, true);
+        pizzaShopService.orderPizza(pizza);
 
     }
 
